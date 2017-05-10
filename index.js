d0a56313872aaeb9102f2388d41539b93763cdaf
ggoha <ryznikov@mail.ru>
2017-05-10 15:46:59 +0300

    add params: width, height, action

--- index.js
+++ index.js
@@ -2,19 +2,22 @@
 var webshot = require('webshot');
 var args = require('minimist')(process.argv.slice(2));
 let presID = args.presID;
+let width = args.width;
+let height = args.height;
+let action = args.action
 
 //webshot('http://of.ru/ex.html', 'google2.png', 
 //webshot(`http://localhost:9875/?id=${presID}`, `map_for_pres${presID}`, 
 console.log(presID);
-webshot(`http://localhost:3002/api/presentations/data/pdf_map?id=${presID}`, `/Users/costa/Sophie/new-api/preses/map_for_pres${presID}.png`, 
+webshot(`http://localhost:3002/api/presentations/data/${action}?id=${presID}`, `/home/ggoha/Документы/new-api/maps/map_for_pres${presID}.png`, 
 //webshot('file:///Users/costa/Sophie/get_ya/ex.html', 'google11.png', 
 //webshot('http://ya.ru', 'google2.png', 
 	{
 		takeShotOnCallback: true,
 		renderDelay: 3000, 
 		screenSize: {
-    		width: 2048, 
-    		height: 1536
+    		width: width, 
+    		height: height
   		},
   		//timeout: 20*1000,
 
